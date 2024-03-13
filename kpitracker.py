c442d8403a3e2e55f18a9ad80350fd9b4e5851ca
nyaridorina <nyaridorina@gmail.com>
2024-03-13 17:01:24 +0100

    03131701

--- kpitracker.py
+++ kpitracker.py
@@ -1,38 +1,24 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template
 from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
-@app.route("/calculate_countback_counter", methods=["POST"])
-def calculate_countback_counter_route():
-    input_date = request.json["input_date"]
-    counter = calculate_countback_counter(input_date)
-    return jsonify({"counter": counter})
+@app.route('/', methods=['GET', 'POST'])
+def calculate_remaining_time():
+    remaining_time_str = ''
+    if request.method == 'POST':
+        arrival_time_str = request.form.get('date')
+        date_format = "%Y-%m-%d %H:%M:%S"
+        arrival_time = datetime.strptime(arrival_time_str, date_format)
+        response_limit = timedelta(hours=8)
+        current_time = datetime.now()
+        time_passed = current_time - arrival_time
+        remaining_time = response_limit - time_passed
+        remaining_hours = remaining_time.seconds // 3600
+        remaining_minutes = (remaining_time.seconds // 60) % 60
+        remaining_time_str = f"You have {remaining_hours} hours and {remaining_minutes} minutes remaining to respond."
 
-def calculate_countback_counter(input_date):
-    # Define working hours
-start_time = datetime.strptime("2024-03-07 08:00:00", "%Y-%m-%d %H:%M:%S").time()
-end_time = datetime.strptime("2024-03-07 16:30:00", "%Y-%m-%d %H:%M:%S").time()
+    return render_template('index.html', remaining_time=remaining_time_str)
 
-    # Convert input date to datetime object
-    input_datetime = datetime.strptime(input_date, "%Y-%m-%d %H:%M:%S")
-
-    # Initialize counter
-    counter = 0
-
-    while input_datetime > datetime.now():
-        # Check if it's a weekday and within working hours
-        if input_datetime.weekday() < 5 and start_time <= input_datetime.time() <= end_time:
-            counter += 1
-        
-        # Decrease one day from the input date
-        input_datetime -= timedelta(days=1)
-
-        # Set the time to the end of the working day if it falls outside working hours
-        if input_datetime.time() > end_time:
-            input_datetime = input_datetime.replace(hour=end_time.hour, minute=end_time.minute, second=end_time.second)
-
-    return counter
-
-if __name__ == "__main__":
-    app.run(debug=True)  # Run the app in debug mode for development
+if __name__ == '__main__':
+    app.run(debug=False)
